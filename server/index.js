603a6f0ec4aff92bd24c5c222d02e872c619b411
Cristian Xavier Tapia Vargas <thecristianx@hotmail.com>
2022-09-23 16:15:50 -0500

    Por solucionar error de express-request-id

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const requestId = require('express-request-id')();
 
 const logger = require('./config/logger');
 
+// Inciciando app
 const app = express();
 
 // Configurando middleware
+app.use(requestId);
 app.use(
   morgan('combined', { stream: { write: (message) => logger.info(message) } })
 );
