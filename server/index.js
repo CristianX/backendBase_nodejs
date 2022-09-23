d9af3bff04b518cf64025d62e75e93e17101ee04
Cristian Xavier Tapia Vargas <thecristianx@hotmail.com>
2022-09-23 15:12:17 -0500

    Añadiendo middleware de manejo de errores

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res, next) => {
-  res.send('Hello World!');
+  res.json(
+    'Bienvenido al backend del Sistema de Remates Judiciales del Municipio de Quito'
+  );
 });
 
 // Ruta no encontrada
@@ -11,6 +13,16 @@ app.use((req, res, next) => {
   res.status(404);
   res.json({
     message: 'Error: Ruta no encontrada',
+  });
+});
+
+// Manejo de errores
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode);
+  res.json({
+    message,
   });
 });
 
