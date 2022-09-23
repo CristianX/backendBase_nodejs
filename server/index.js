0afda5723e063bb6086ad8174ebaa5338b48a75f
Cristian Xavier Tapia Vargas <thecristianx@hotmail.com>
2022-09-23 14:18:55 -0500

    Añadiendo middleware para mensaje de error si no encuentra una ruta

--- server/index.js
+++ server/index.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.send('Hello World!');
+});
+
+// Ruta no encontrada
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({
+    message: 'Error: Ruta no encontrada',
+  });
 });
 
 module.exports = app;
